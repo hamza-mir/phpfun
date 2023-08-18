5e1be19c2b97f8c90cadfde2fab888c991993a79
hamzamir66 <39001154+hamzamir66@users.noreply.github.com>
2023-08-18 22:56:34 +0000

    null dtype

--- index.php
+++ index.php
@@ -8,11 +8,10 @@ This is also a comment
 # Single line comment
 echo "<h1>Hello World</h1>";
 echo "My name is Hmaza";
-
 $age = 29;
-
 $age += 1;
-
 echo $age;
 echo "<br/>";
-var_dump(29);
+
+$data = null;
+var_dump($data);
