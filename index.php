f2f03b5d30b0b16c926cf1876782eee6160f1a43
hamzamir66 <39001154+hamzamir66@users.noreply.github.com>
2023-08-20 03:24:48 +0000

    int and float

--- index.php
+++ index.php
@@ -6,14 +6,19 @@ Multi line commment
 This is also a comment
 */
 # Single line comment
-echo "<h1>Hello World</h1>";
-echo "My name is Hmaza";
-$age = 29;
-$age += 1;
-echo $age;
-echo "<br/>";
+// echo "<h1>Hello World</h1>";
+// echo "My name is Hmaza";
+// $age = 29;
+// $age += 1;
+// echo $age;
+// echo "<br/>";
 
 $data = null; // null dtype
 
-$isLoggedIn = true;
-var_dump($isLoggedIn);
+$isLoggedIn = true; // bool dtype
+
+$floatNumber = 50.4;
+$integerNumber = 5_000;
+var_dump($floatNumber);
+echo "<br/>";
+var_dump($integerNumber);
