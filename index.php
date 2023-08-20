c8fdadfc199f2aa70eb9977b89a72596f9a8914f
hamzamir66 <39001154+hamzamir66@users.noreply.github.com>
2023-08-20 03:59:18 +0000

    typecasting

--- index.php
+++ index.php
@@ -13,28 +13,71 @@ This is also a comment
 // echo $age;
 // echo "<br/>";
 
-$data = null; // null dtype
+// $data = null; // null dtype
 
-$isLoggedIn = true; // bool dtype
+// $isLoggedIn = true; // bool dtype
 
-$floatNumber = 50.4;
-$integerNumber = 5_000;
+// $floatNumber = 50.4;
+// $integerNumber = 5_000;
 
-$name = 'Hamza';
-$welcome = "welcome {$name}";
-$welcome[0] = 'W';
+// $name = 'Hamza';
+// $welcome = "welcome {$name}";
+// $welcome[0] = 'W';
 
-$arrayExample = ['Salad', 'Burger', 'Pizza'];
-$arrayExample[1] = 'Chicken';
-$arrayExample[] = 'Soup';
+// $arrayExample = ['Salad', 'Burger', 'Pizza'];
+// $arrayExample[1] = 'Chicken';
+// $arrayExample[] = 'Soup';
 
-$arrayExample = [
-	'john' => ['Salad', 'Spaghetti'],
-	'jane' => 'Burger',
-	'sam' => 'Pizza'
-];
-$arrayExample['jane'] = 'Chicken';
-$arrayExample['bob'] = 'Soup';
+// $arrayExample = [
+// 	'john' => ['Salad', 'Spaghetti'],
+// 	'jane' => 'Burger',
+// 	'sam' => 'Pizza'
+// ];
+// $arrayExample['jane'] = 'Chicken';
+// $arrayExample['bob'] = 'Soup';
 
+// Booleans 
+// var_dump((bool) "");             // -> false
+// var_dump((bool) "Some Text");    // -> true
+// var_dump((boolean) "0");         // -> false
+// var_dump((bool) "false");        // -> true
+// var_dump((bool) 0);              // -> false
+// var_dump((bool) 1);              // -> true
+// var_dump((bool) -1);             // -> true
+// var_dump((bool) null);           // -> false
+// var_dump((bool) []);             // -> false
+// var_dump((bool) ["hello"]);      // -> true
 
-var_dump($arrayExample['john'][0]);
+// Integers
+// var_dump((int) false);        // -> 0
+// var_dump((integer) true);     // -> 1
+// var_dump((int) "-134");       // -> -1
+// var_dump((int) "Hello");      // -> 0
+// var_dump((int) "12 months");  // -> 12
+// var_dump((int) 12.7);         // -> 12
+// var_dump((int) null);         // -> 0
+
+// Float
+// var_dump((float) false);      // -> 0
+// var_dump((float) true);       // -> 1
+// var_dump((float) "-1");       // -> -1
+// var_dump((float) "Hello");    // -> 0
+// var_dump((float) "2.5 Hour"); // -> 2.5
+// var_dump((float) null);       // -> 0
+
+// Strings
+// var_dump((string) false);     // -> ""
+// var_dump((string) true);      // -> "1"
+// var_dump((string) 0);         // -> "0"
+// var_dump((string) 1.353);     // -> "1.353"
+// var_dump((string) []);        // -> "Array"
+// var_dump((string) ["John"]);  // -> "Array"
+// var_dump((string) null);      // -> ""
+
+// Arrays
+// var_dump((array) false);      // -> [false]
+// var_dump((array) true);       // -> [true]
+// var_dump((array) 0);          // -> [0]
+// var_dump((array) 1.353);      // -> [1.353]
+// var_dump((array) "John");     // -> ["John"]
+// var_dump((array) null);       // -> []
